058e9172824d6d959d7f47fed310e94dd80a920d
srin <smadipalli@hotmail.com>
2015-03-10 14:46:22 +0000

    fog config

--- Gemfile
+++ Gemfile
@@ -24,6 +24,7 @@ gem 'fog-aws', '~> 0.1.1'
 gem 'will_paginate', '~> 3.0.7'
 gem 'will_paginate-bootstrap', '~> 1.0.1'
 
+
 group :development, :test do
      gem 'sqlite3'
 end
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,5 +1,3 @@
-CarrierWave.configure do |config|
-
   # Use local storage if in development or test
   if Rails.env.development? || Rails.env.test?
     CarrierWave.configure do |config|
@@ -18,11 +16,11 @@ CarrierWave.configure do |config|
     :provider               => 'AWS',                             # required
     :aws_access_key_id      => 'AKIAIYSXGV62HOFJP6CQ',            # required
     :aws_secret_access_key  => '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',     # required
-    :region                 => 'eu-west-1'                        # optional, defaults to 'us-east-1'
+    # :region                 => 'eu-west-1'                        # optional, defaults to 'us-east-1'
   }
   config.fog_directory  = 'dhproperty'               # required
   #config.fog_host       = 'https://assets.example.com'           # optional, defaults to nil
   #config.fog_public     = false                                  # optional, defaults to true
-  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+  # config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
 
