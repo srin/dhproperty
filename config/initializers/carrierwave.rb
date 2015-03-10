f90c40ecda53929ceb77186d6e54119eda2317f7
srin <smadipalli@hotmail.com>
2015-03-10 15:09:17 +0000

    fog

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,3 +1,5 @@
+CarrierWave.configure do |config|
+
   # Use local storage if in development or test
   if Rails.env.development? || Rails.env.test?
     CarrierWave.configure do |config|
@@ -22,5 +24,5 @@
   #config.fog_host       = 'https://assets.example.com'           # optional, defaults to nil
   #config.fog_public     = false                                  # optional, defaults to true
   # config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
-
+end
 
